fcb8b2953396f5f08d7adece2d8aff33ca483a1a
Feyza Basaran <basa0044@algonquinlive.com>
2022-10-16 21:34:57 -0400

    completes the assignment

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
 # mtm6302-emoji-basa0044
+
+Name: Feyza Basaran
+
+Student Number: 040924173
---
+++ script.js
@@ -0,0 +1,11 @@
+const emoji = [ ]
+
+for (let i = 128512 ; i <= 128586; i++){
+    emoji.push(/*html*/ `<p style="text-align: center;"><span 
+    style="font-size: 5rem;">&#${i};</span><br><code>${i}</code></p>`)
+}
+
+const $body = document.getElementById('body')
+
+$body.innerHTML += emoji.join('')
+
