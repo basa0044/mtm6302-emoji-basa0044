53c47a02223cf2da7186d556b8a08e3036cade42
Feyza Basaran <basa0044@algonquinlive.com>
2022-10-19 21:44:48 -0400

    completes the assignment

--- script.js
+++ script.js
@@ -1,6 +1,11 @@
 const emoji = [ ]
 
-for (let i = 128512 ; i <= 128586; i++){
+for (let i = 128506 ; i <= 128591; i++){
+    emoji.push(/*html*/ `<p class=emoji style="text-align: center;"><span 
+    style="font-size: 5rem;">&#${i};</span><br><code>${i}</code></p>`)
+}
+
+for (let i = 128640 ; i <= 128653; i++){
     emoji.push(/*html*/ `<p class=emoji style="text-align: center;"><span 
     style="font-size: 5rem;">&#${i};</span><br><code>${i}</code></p>`)
 }
